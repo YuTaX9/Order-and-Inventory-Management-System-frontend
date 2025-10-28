98111af7aaba4b3b9c2daa680e81ff30e7d9bfc0
Bassam Alghamdi <bassam.ghk@gmail.com>
2025-10-28 13:23:25 +0300

    Create Loading and ErrorMessage components

---
+++ src/components/common/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center h-64">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500">Loading...</div>
+    </div>
+  );
+};
+
+export default Loading;
